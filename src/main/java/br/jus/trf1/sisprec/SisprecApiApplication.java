172b090410f1030c755220a138d09d025842b5c6
limarcelo9 <limarcelo90@gmail.com>
2024-06-12 20:58:51 -0300

    commit inicial sisprec

---
+++ src/main/java/br/jus/trf1/sisprec/SisprecApiApplication.java
@@ -0,0 +1,13 @@
+package br.jus.trf1.sisprec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisprecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisprecApiApplication.class, args);
+	}
+
+}
